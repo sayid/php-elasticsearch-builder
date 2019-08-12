97dce96d6e1f6f91c659b8fe3da0ae0bccf8d848
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-08-12 11:12:55 +0100

    chore: Init
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ .php_cs.php
@@ -0,0 +1,29 @@
+<?php
+
+// Directories to not scan
+$excludeDirs = [
+    'vendor/',
+];
+
+// Files to not scan
+$excludeFiles = [];
+
+// Create a new Symfony Finder instance
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude($excludeDirs)
+    ->ignoreDotFiles(true)->ignoreVCS(true)
+    ->filter(function (\SplFileInfo $file) use ($excludeFiles) {
+        return ! in_array($file->getRelativePathName(), $excludeFiles);
+    })
+;
+
+return Hypefactors\CodeStandards\Config::create()
+    ->setFinder($finder)
+    ->setUsingCache(false)
+    ->setRiskyAllowed(true)
+    ->withPHPUnitRules()
+    ->setRules([
+        'php_unit_strict' => false,
+    ])
+;

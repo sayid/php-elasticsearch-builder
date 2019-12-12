86558f9d45e1fe62b40dd765cad20cc066ed1474
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-12-12 15:58:14 +0000

    chore: Add Sum Aggregation
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Aggregation/Metrics/SumAggregation.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hypefactors\ElasticBuilder\Aggregation\Metrics;
+
+use InvalidArgumentException;
+use Hypefactors\ElasticBuilder\Core\Util;
+use Hypefactors\ElasticBuilder\Aggregation\Aggregation;
+
+/**
+ * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-metrics-sum-aggregation.html
+ */
+class SumAggregation extends Aggregation
+{
+    public function field(string $field): self
+    {
+        $this->body['field'] = $field;
+
+        return $this;
+    }
+
+    public function getBody(): array
+    {
+        if (! isset($this->body['field'])) {
+            throw new InvalidArgumentException('The "field" is required!');
+        }
+
+        return [
+            'sum' => Util::recursivetoArray($this->body),
+        ];
+    }
+}
